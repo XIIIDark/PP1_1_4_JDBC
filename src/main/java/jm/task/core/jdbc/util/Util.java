8837dad929fbcedc9d8e36c4f101e35c60647756
XIIIDark <alexartemyev13@gmail.com>
2024-02-04 01:19:05 +0600

    реализация JDBC

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,36 @@
+package jm.task.core.jdbc.util;
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String HOST_NAME = "localhost";
+    private static final String DB_NAME = "myschemas";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "uGV!hu3.24sdf";
+    private static Connection conn = null;
+
+    public static Connection getMySQLConnection() {
+        return conn;
+    }
+
+    public static void closeMySQLConnection() throws SQLException {
+        conn.close();
+    }
+
+    public static void initMySQLConnection() {
+
+        String connectionURL = "jdbc:mysql://" + HOST_NAME + ":3306/" + DB_NAME;
+
+        try {
+            conn =  DriverManager.getConnection(connectionURL, USER_NAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
